b997105cfc45edbad78ddae713c4ccbc45bfec76
Juanzcc <juanzcc12@hotmail.com>
2021-03-24 17:58:17 -0300

    Criado o MVC - Departamento

---
+++ ProjetoAspNet/Controllers/DepartamentoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjetoAspNet.Models;
+
+namespace ProjetoAspNet.Controllers
+{
+    public class DepartamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamento> list = new List<Departamento>();
+            list.Add(new Departamento { Id = 1, Nome = "Eletronics" });
+            list.Add(new Departamento { Id = 2, Nome = "Fashion" });
+
+            return View(list);
+        }
+    }
+}
