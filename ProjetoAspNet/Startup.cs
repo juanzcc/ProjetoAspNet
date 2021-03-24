175c879d841864aaeb4c545c0ae5c510f42b2812
Juanzcc <juanzcc12@hotmail.com>
2021-03-24 19:36:52 -0300

    Adicionando BD MySql

--- ProjetoAspNet/Startup.cs
+++ ProjetoAspNet/Startup.cs
@@ -37,7 +37,7 @@ namespace ProjetoAspNet
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ProjetoAspNetContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ProjetoAspNetContext")));
+                    options.UseMySql(Configuration.GetConnectionString("ProjetoAspNetContext"), builder => builder.MigrationsAssembly("ProjetoAspNet")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
